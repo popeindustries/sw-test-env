a13f4d0d69d3a0db7683f465e0c9c4b49c72780b
Alexander Pope <alex@pope-industries.com>
2017-07-09 18:35:59 +0200

    only run workbox test on Node 8

--- test/6-integration-test.js
+++ test/6-integration-test.js
@@ -5,17 +5,19 @@ const { connect, destroy } = require('../index');
 
 let sw;
 
-describe('workbox-sw', () => {
-  beforeEach(() => {
-    sw = connect('http://localhost:3333', process.cwd());
-  });
-  afterEach(() => {
-     destroy();
-  });
+if (process.version.charAt(1) === '8') {
+  describe('workbox-sw', () => {
+    beforeEach(() => {
+      sw = connect('http://localhost:3333', process.cwd());
+    });
+    afterEach(() => {
+      destroy();
+    });
 
-  it('should initialize a workbox instance', () => {
-    return sw.register('./fixtures/sw-workbox.js').then(registration => {
-      expect(sw.scope.workboxSW).to.have.property('_router');
+    it('should initialize a workbox instance', () => {
+      return sw.register('./fixtures/sw-workbox.js').then(registration => {
+        expect(sw.scope.workboxSW).to.have.property('_router');
+      });
     });
   });
-});
+}
