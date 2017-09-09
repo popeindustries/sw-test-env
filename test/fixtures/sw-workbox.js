b75adb5740cdc8f2e3275f2b4e81c0911ef9aa79
Alexander Pope <alex@pope-industries.com>
2017-09-09 14:38:30 +0200

    fix test

--- test/fixtures/sw-workbox.js
+++ test/fixtures/sw-workbox.js
@@ -1,4 +1,4 @@
-importScripts('node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v1.0.1.js');
+importScripts('node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.0.1.js');
 
 self.workboxSW = new WorkboxSW({
   clientsClaim: true,
