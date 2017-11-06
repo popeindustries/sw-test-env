75113dc218e3a02b48dacb966e419d17295dba10
Alexander Pope <alex@pope-industries.com>
2017-11-06 15:01:41 +0100

    upgrade dependencies

--- test/fixtures/sw-workbox.js
+++ test/fixtures/sw-workbox.js
@@ -1,7 +1,6 @@
-importScripts('node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.0.1.js');
+importScripts('node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.1.js');
 
 self.workboxSW = new WorkboxSW({
   clientsClaim: true,
-  skipWaiting: true,
+  skipWaiting: true
 });
-
